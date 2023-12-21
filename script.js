fa5fc9a35be3c4eaa0cefcb457e250d3f6fef5eb
dustvans <dustinevans07@gmail.com>
2023-12-20 20:50:58 -0600

    Created dynamic layout, ability to change layout size

--- script.js
+++ script.js
@@ -1,22 +1,28 @@
+createGrid();
 
-
-function createGrid() {
+function createGrid(size = 16) {
     const grid = document.querySelector('#grid');
+    grid.innerHTML = '';
 
-    for (let index = 0; index < 256; index++) {
+    for (let index = 0; index < (size * size); index++) {
         const gridElement = document.createElement('div');
-        gridElement.style.outline = '1px solid red';
-        gridElement.style.width = '25px';
-        gridElement.style.height = '25px';
+        gridElement.classList.add('grid-item');
+
+        gridElement.style.width = `${(400 / size)}px`;
+        gridElement.style.height = `${(400 / size)}px`;
+
         grid.appendChild(gridElement);
-        
     }
-
-    grid.style.display = 'flex';
-    grid.style.flexWrap = 'wrap';
-    grid.style.width = '400px';
-
 
 }
 
-createGrid();
+const changeLayout = document.querySelector('#change-layout');
+changeLayout.addEventListener('click', () =>{
+    let newLayout = prompt("New layout size? Enter the amount of squares on one side:")
+
+    while (newLayout > 100) {
+        newLayout = prompt("New layout size must be less than 100, try again:");
+    }
+
+    createGrid(newLayout)
+});
